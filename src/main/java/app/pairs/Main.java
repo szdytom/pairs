ebac7bae4b3542446fd57d0ac78b1df5009080f6
szdytom <szdytom@qq.com>
2026-04-14 16:47:30 +0800

    ui: dep: introduce libsdl4j as render library

--- src/main/java/app/pairs/Main.java
+++ src/main/java/app/pairs/Main.java
@@ -1,29 +1,74 @@
 package app.pairs;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
+import io.github.libsdl4j.api.event.SDL_Event;
+import io.github.libsdl4j.api.render.SDL_Renderer;
+import io.github.libsdl4j.api.video.SDL_Window;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		Rectangle rectangle = new Rectangle(50, 50, 200, 100);
-		rectangle.setFill(Color.LIGHTBLUE);
-		rectangle.setStroke(Color.BLUE);
-		rectangle.setStrokeWidth(3);
+import static io.github.libsdl4j.api.Sdl.SDL_Init;
+import static io.github.libsdl4j.api.Sdl.SDL_Quit;
+import static io.github.libsdl4j.api.SdlSubSystemConst.SDL_INIT_EVERYTHING;
+import static io.github.libsdl4j.api.error.SdlError.SDL_GetError;
+import static io.github.libsdl4j.api.event.SDL_EventType.*;
+import static io.github.libsdl4j.api.event.SdlEvents.SDL_PollEvent;
+import static io.github.libsdl4j.api.keycode.SDL_Keycode.SDLK_SPACE;
+import static io.github.libsdl4j.api.render.SDL_RendererFlags.SDL_RENDERER_ACCELERATED;
+import static io.github.libsdl4j.api.render.SdlRender.*;
+import static io.github.libsdl4j.api.video.SDL_WindowFlags.SDL_WINDOW_RESIZABLE;
+import static io.github.libsdl4j.api.video.SDL_WindowFlags.SDL_WINDOW_SHOWN;
+import static io.github.libsdl4j.api.video.SdlVideo.SDL_CreateWindow;
+import static io.github.libsdl4j.api.video.SdlVideoConst.SDL_WINDOWPOS_CENTERED;
 
-		Pane root = new Pane(rectangle);
-		Scene scene = new Scene(root, 400, 300);
+public class Main {
+    public static void main(String[] args) {
+        // Initialize SDL
+        int result = SDL_Init(SDL_INIT_EVERYTHING);
+        if (result != 0) {
+            throw new IllegalStateException("Unable to initialize SDL library (Error code " + result + "): " + SDL_GetError());
+        }
 
-		primaryStage.setTitle("P.A.I.R.S.");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+        // Create and init the window
+        SDL_Window window = SDL_CreateWindow("Demo SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+        if (window == null) {
+            throw new IllegalStateException("Unable to create SDL window: " + SDL_GetError());
+        }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        // Create and init the renderer
+        SDL_Renderer renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (renderer == null) {
+            throw new IllegalStateException("Unable to create SDL renderer: " + SDL_GetError());
+        }
+
+        // Set color of renderer to green
+        SDL_SetRenderDrawColor(renderer, (byte) 0, (byte) 255, (byte) 0, (byte) 255);
+
+        // Clear the window and make it all red
+        SDL_RenderClear(renderer);
+
+        // Render the changes above ( which up until now had just happened behind the scenes )
+        SDL_RenderPresent(renderer);
+
+        // Start an event loop and react to events
+        SDL_Event evt = new SDL_Event();
+        boolean shouldRun = true;
+        while (shouldRun) {
+            while (SDL_PollEvent(evt) != 0) {
+                switch (evt.type) {
+                    case SDL_QUIT:
+                        shouldRun = false;
+                        break;
+                    case SDL_KEYDOWN:
+                        if (evt.key.keysym.sym == SDLK_SPACE) {
+                            System.out.println("SPACE pressed");
+                        }
+                        break;
+                    case SDL_WINDOWEVENT:
+                        System.out.println("Window event " + evt.window.event);
+                    default:
+                        break;
+                }
+            }
+        }
+
+        SDL_Quit();
+    }
 }
