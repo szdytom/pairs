d2c5d2fa0cffb8d76510db6343f67f80c4e473cc
Steven777 <stevvevven@gmail.com>
2026-04-29 17:26:49 +0800

    map: change some parameter in hard mode to make hard mode harder.

--- src/main/java/app/pairs/map/HardTilemapFactory.java
+++ src/main/java/app/pairs/map/HardTilemapFactory.java
@@ -5,8 +5,8 @@ import app.pairs.model.Tilemap;
 import java.util.Random;
 
 public class HardTilemapFactory implements TilemapFactory {
-	private static final int WIDTH = 16;
-	private static final int HEIGHT = 16;
+	private static final int WIDTH = 12;
+	private static final int HEIGHT = 12;
 	private static final int TYPES = 20;
 
 	private final Random random = new Random();
