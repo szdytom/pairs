52beb07bf67ac797cb5229c4b9055d39e5955181
szdytom <szdytom@qq.com>
2026-05-09 15:59:50 +0800

    view: background: add Background class for rendering background color
    Signed-off-by: szdytom <szdytom@qq.com>

--- src/main/java/app/pairs/view/Button.java
+++ src/main/java/app/pairs/view/Button.java
@@ -1,37 +1,17 @@
 package app.pairs.view;
 
-public class Button extends Container {
+import static app.pairs.utils.Colors.*;
+
+public class Button extends Background {
 	private final Runnable onClick;
-	private final int[] measuredSize = new int[2];
 
 	public Button(Runnable onClick) {
+		this(onClick, rgba(0, 0, 0, 0), rgba(0, 0, 0, 20));
+	}
+
+	public Button(Runnable onClick, int color, int hoverColor) {
+		super(color, hoverColor);
 		this.onClick = onClick;
-	}
-
-	@Override
-	public int[] measure() {
-		int maxW = 0;
-		int maxH = 0;
-		for (Widget child : children) {
-			int[] s = child.measure();
-			if (s[0] > maxW) {
-				maxW = s[0];
-			}
-			if (s[1] > maxH) {
-				maxH = s[1];
-			}
-		}
-		measuredSize[0] = maxW;
-		measuredSize[1] = maxH;
-		return measuredSize;
-	}
-
-	@Override
-	public void layout(int x, int y, int w, int h) {
-		super.layout(x, y, w, h);
-		for (Widget child : children) {
-			child.layout(0, 0, w, h);
-		}
 	}
 
 	@Override
