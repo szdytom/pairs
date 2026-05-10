8de52de0c47ac4aa86a91c1cabc9242a889a1bdd
szdytom <szdytom@qq.com>
2026-05-10 16:40:07 +0800

    model: game status: track auto-solver item count
    Signed-off-by: szdytom <szdytom@qq.com>

--- src/main/java/app/pairs/model/GameStatus.java
+++ src/main/java/app/pairs/model/GameStatus.java
@@ -2,9 +2,11 @@ package app.pairs.model;
 
 public class GameStatus {
 	public int score;
+	public int autoSolvers;
 
 	public GameStatus() {
 		this.score = 0;
+		this.autoSolvers = 1;
 	}
 
 	public void changeScore(int delta) {
